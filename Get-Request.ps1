13fc526ca177b9f55f40f744b9140350e9b47132
devopscollective <39610169+devopscollective@users.noreply.github.com>
2018-05-27 11:20:19 -0500

    Get-Request script added
    Script to poll local or remote host for specific software installation location and corresponding downloaded file locations on the C: drive.

---
+++ Get-Request.ps1
@@ -0,0 +1,141 @@
+﻿<#
+
+    # Synopsis
+
+    Function to search C: for a specific file name.  
+
+    
+
+    # Example
+
+    Get-AppPath
+
+#>
+Function Get-AppPath {
+
+ 
+
+$pc = Read-Host "Computer Name"
+
+ 
+
+$app = Read-Host "Application Name"
+
+ 
+
+$found = @()
+
+ 
+
+foreach($computer in $pc) {
+
+ 
+
+$file = Get-WMiObject -ComputerName $computer -Class CIM_DataFile -Filter "FileName = '$app'"
+
+ 
+
+$file | Add-Member -MemberType NoteProperty -Name ComputerName -Value $computer
+
+ 
+
+$found += $file
+
+ 
+
+}
+
+ 
+
+$found | Out-GridView
+
+ 
+
+}
+
+ 
+
+<#
+
+    # Synopsis
+
+    Function to poll all registry locations for installed software
+
+    
+
+    # Example
+
+    Get-InstalledSoftware -ComputerName dcenocis198*** -DisplayName *mozilla*
+
+#>
+function Get-InstalledSoftware {
+
+ 
+
+    param
+
+    (
+
+        $DisplayName='*',
+
+ 
+
+        $DisplayVersion='*',
+
+ 
+
+        $UninstallString='*',
+
+ 
+
+        $InstallDate='*'
+
+ 
+
+    )
+
+   
+
+    # registry locations where installed software is logged
+
+    $pathAllUser = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
+
+    $pathCurrentUser = "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
+
+    $pathAllUser32 = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+
+    $pathCurrentUser32 = "Registry::HKEY_CURRENT_USER\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+
+ 
+
+  
+
+    # get all values
+
+    Get-ItemProperty -Path $pathAllUser, $pathCurrentUser, $pathAllUser32, $pathCurrentUser32 |
+
+      # choose the values to use
+
+      Select-Object -Property DisplayVersion, DisplayName, UninstallString, InstallDate |
+
+      # skip all values w/o displayname
+
+      Where-Object DisplayName -ne $null |
+
+      # apply user filters submitted via parameter:
+
+      Where-Object DisplayName -like $DisplayName |
+
+      Where-Object DisplayVersion -like $DisplayVersion |
+
+      Where-Object UninstallString -like $UninstallString |
+
+      Where-Object InstallDate -like $InstallDate |
+
+ 
+
+      # sort by displayname
+
+      Sort-Object -Property DisplayName
+
+}
